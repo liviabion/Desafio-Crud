d487459929656e65bfbaece246e5c7f4f24df849
PaulaPerazzo <mpperazzosampaio@gmail.com>
2022-11-25 18:14:30 -0300

    challenge conf

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -15,34 +15,5 @@ export default class UserController implements Crud {
 
         return response.status(httpStatus).send({ message });
     }
-
-    async get(request: Request, response: Response){
-        const {httpStatus, values} = await Citi.getAll(User);
-        return response.status(httpStatus).send(values);
-    }
-
-    async delete(request: Request, response: Response){
-        const { id } = request.params;
-        const {value: userFound, message } = await Citi.findByID(User, id); 
-           
-        if(!userFound) return response.status(400).send({ message });
-
-        const {httpStatus, messageFromDelete } = await Citi.deleteValue(User, userFound);
-        return response.status(httpStatus).send({ messageFromDelete });
-    }
-
-    async update(request: Request, response: Response){
-        const { id } = request.params;
-        const {firstName, lastName, age } = request.body;
-
-        const isAnyUndefined = Citi.areValuesUndefined(firstName, lastName, age, id);
-        if(isAnyUndefined) return response.status(400).send();
-
-        const userWithUpdatedValues = { firstName, lastName, age };
-
-        const { httpStatus, messageFromUpdate } = await Citi.updateValue(User, id, userWithUpdatedValues);
-        return response.status(httpStatus).send({ messageFromUpdate });
-    }
-
     
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,9 +5,5 @@ const routes = express.Router();
 const userController = new UserController();
 
 routes.post('/user', userController.create);
-routes.get('/user', userController.get);
-routes.delete('/user/:id', userController.delete);
-routes.put('/user/:id', userController.update);
-
 
 export default routes;
